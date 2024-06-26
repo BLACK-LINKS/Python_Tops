1e8bd6177d4b8db0ba56b7962dcde800d4ac175b
BLACK-LINKS <blacklink1994@proton.me>
2024-06-26 16:33:52 +0530

    26-06-2024

--- bank_task.py
+++ bank_task.py
@@ -1,53 +1,53 @@
 import random
-
-acno=random.randint(100000,999999)
-
 class Bank:
-    def openAccount(self,acno,cname,balance):
-        self.acno=acno
-        self.cname=cname
-        self.balance=balance
-        print("Hello ",cname," Your account is opened. ",acno," This is your account number with ",balance," rs.")  
-    
-    file=open(acno+".txt", "w")
-    ac=str(acno)
-    name=cname
-    bal=str(balance)
+    def __init__(self):
+        self.acno = None
+        self.cname = None
+        self.balance = None
 
-    file.write(ac)
-    file.write(name)
-    file.write(bal)
-    file.close()
-    
-    def deposit(self,amount):
-        self.balance+=amount
+    def openAccount(self, cname, balance):
+        self.acno = random.randint(100000, 999999)  # Generate 6-digit random account number
+        self.cname = cname
+        self.balance = balance
+        self.recordTransaction("Account Opened", balance)
+        print("Hello ", cname, " Your account is opened. ", self.acno, " This is your account number with ", balance, " rs.")
 
-    file=open(acno+".txt", "w")
-    amount=str(amount)
-    file.write("DEPOSIT AMOUNT : ",amount)
-    file.close()
+    def deposit(self, amount):
+        self.balance += amount
+        self.recordTransaction("Deposit", amount)
+        print("Deposit successful. New balance:", self.balance)
 
-    def withdraw(self,withdraw):
-        if amount<=self.balance:
-            self.balance-=amount
-            file=open(acno+".txt", "w")
-            amount=str(amount)
-            file.write("Withdraw Amount: ",amount)
-            file.close()
-                
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            self.recordTransaction("Withdraw", amount)
+            print("Withdrawal successful. New balance:", self.balance)
         else:
-            print("Insufficient Balance ",amount-self.balance, " rs. More is required. ")
+            print("Insufficient Balance ", amount - self.balance,"rs. More is required. ")
 
     def checkbalance(self):
-        self.balance
-    
-        
-b1=Bank()
-acno=int(input("Enter Account Number : "))
-cname=input("Enter Acc Holder Name : ")
-balance=int(input("Enter Initial Balance : "))
+        self.recordTransaction("Check Balance", self.balance)
+        print("Your current balance is:", self.balance)
 
-b1.openAccount(acno,cname,balance)
+    def recordTransaction(self, transactionType, amount):
+        filename = str(self.acno) + ".txt"
+        file = open(filename, "a")
+        file.write(transactionType + ": " + str(amount) + "\n")
+        file.close()
+
+    def displayTransactions(self):
+        filename = str(self.acno) + ".txt"
+        file = open(filename, "r")
+        print("Transaction History:")
+        print(file.read())
+        file.close()
+
+b1 = Bank()
+
+cname = input("Enter Acc Holder Name : ")
+balance = int(input("Enter Initial Balance : "))
+
+b1.openAccount(cname, balance)
 
 while True:
     print("*********************************")
@@ -57,28 +57,25 @@ while True:
     print("4. Exit")
     print("*********************************")
 
-    choice=int(input("Enter your choice: "))
+    choice = int(input("Enter your choice: "))
     print("*********************************")
 
-    if choice==1:
-        amount=int(input("Enter Deposit Amount: "))
+    if choice == 1:
+        amount = int(input("Enter Deposit Amount: "))
         b1.deposit(amount)
-        
-    elif choice==2:
-        amount=int(input("Enter Withdrawal Amount: "))
+
+    elif choice == 2:
+        amount = int(input("Enter Withdrawal Amount: "))
         b1.withdraw(amount)
 
-    elif choice==3:
+    elif choice == 3:
         b1.checkbalance()
 
-    elif choice==4:
+    elif choice == 4:
+        b1.recordTransaction("Exit", 0)  # Record system exit
+        b1.displayTransactions()  # Display transaction history
         print("Thank you for using our Services. ")
         break
 
     else:
         print("Invalid Choice, Please Try again!!")
-
-
-file=open(acno+".txt" , "r")
-print(file.read())
-file.close()
